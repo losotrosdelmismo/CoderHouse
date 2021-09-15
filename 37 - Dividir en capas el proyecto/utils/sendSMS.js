74da11274a659e58ba3ff8444cb26d564860721c
losotrosdelmismo <48542329+losotrosdelmismo@users.noreply.github.com>
2021-09-14 23:25:35 -0300

    Delete 37 - Dividir en capas el proyecto directory

--- 37 - Dividir en capas el proyecto/utils/sendSMS.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-const accountSid = process.env.accountSid;
-const authToken = process.env.authToken;
-
-const client = require('twilio')(accountSid, authToken);
-
-module.exports = client;
